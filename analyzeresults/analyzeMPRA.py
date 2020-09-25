d6e43e1753e54c21e7afaabf235c56e174ad8671
Matthew Taliaferro <taliaferrojm@gmail.com>
2020-09-25 16:20:34 -0600

    added LME

--- analyzeresults/analyzeMPRA.py
+++ analyzeresults/analyzeMPRA.py
@@ -16,9 +16,14 @@ import pandas as pd
 import numpy as np
 from collections import OrderedDict
 from statsmodels.robust.scale import mad as MAD
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 from statsmodels.stats.multitest import multipletests
 from random import shuffle
 from scipy.stats import percentileofscore
+from scipy.stats.distributions import chi2
+import warnings
+import time
 
 def getntcoverage(umicounts, stepsize, oligosize, smoothcoverage):
 	#Here we are going to get nt-level coverage. Oligo names are of the form
@@ -483,18 +488,190 @@ def getpvalues_MADstat(countdf, sampconds, conditionA, conditionB):
 	bigdf = pd.concat([pdf, condameandf, condbmeandf, condamaddf, condbmaddf], axis = 1, join = 'inner')
 	
 	return bigdf
+
+def getpvalues_LME(countdf, sampconds, conditionA, conditionB):
+	#Use a linear mixed effects model to interrogate differences
+	#between conditions
+
+	#in countdf, indicies (rownames) are utr_nt and columns are samples
+	#log2 transform count values
+
+	pvaluedict = OrderedDict() #{utr_nt : pvalue}
+	condameans = []
+	condbmeans = []
+	condamads = []
+	condbmads = []
 	
+	samps = OrderedDict() #{condition : [sample1, sample2, ...]} this needs to be ordered because we will be iterating through it
+	samps['conditionA'] = []
+	samps['conditionB'] = []
+	with open(sampconds, 'r') as infh:
+		for line in infh:
+			line = line.strip().split('\t')
+			if line[0] == 'sample':
+				continue
+			if line[1] == conditionA:
+				samps['conditionA'].append(os.path.basename(line[0]))
+			elif line[1] == conditionB:
+				samps['conditionB'].append(os.path.basename(line[0]))
+
+	condasamps = samps['conditionA']
+	condbsamps = samps['conditionB']
+	allsamps = condasamps + condbsamps
+
+	print('Condition A samples: ' + (', ').join(condasamps))
+	print('Condition B samples: ' + (', ').join(condbsamps))
+
+	#Iterate through rows, making a dictionary from every row, turning it into a dataframe, then calculating p value
+	currentutr = ''
+	for index, row in countdf.iterrows():
+		utr = index.split('_')[0]
+		nt = int(index.split('_')[1])
+		print(nt)
+		if utr != 'Gdf11':
+			continue
+		if utr != currentutr:
+			print('Calculating pvalue for {0}...'.format(utr))
+			currentutr = utr
+
+		d = {}
+		d['nt'] = [index] * len(allsamps)
+		d['variable'] = allsamps
+		
+		values = [] #counts
+		for cond in samps:
+			for sample in samps[cond]:
+				value = row[sample]
+				values.append(value)
+
+		#log2transform
+		pc = 0.01
+		values = [log2(v + pc) for v in values]
+
+		d['value'] = values
+
+		condavalues = values[:len(condasamps)]
+		condbvalues = values[len(condasamps):]
+		condamean = np.mean(condavalues)
+		condbmean = np.mean(condbvalues)
+		condamad = MAD(condavalues, c = 1)
+		condbmad = MAD(condbvalues, c = 1)
+
+		condameans.append(condamean)
+		condbmeans.append(condbmean)
+		condamads.append(condamad)
+		condbmads.append(condbmad)
+
+		#If there is an NA count value, we are not going to calculate a pvalue for this gene
+		p = None
+		if np.isnan(values).any():
+			p = np.nan
+
+		conds = []
+		for cond in samps:
+			conds += [cond] * len(samps[cond])
+		cond1s = []
+		cond2s = []
+		for cond in conds:
+			if cond == 'conditionA':
+				cond1s.append(1)
+				cond2s.append(0)
+			elif cond == 'conditionB':
+				cond1s.append(0)
+				cond2s.append(1)
+
+		d['cond1'] = cond1s #e.g. [1, 1, 1, 1, 0, 0, 0, 0]
+		d['cond2'] = cond2s #e.g. [0, 0, 0, 0, 1, 1, 1, 1]
+		d['samples'] = [x + 1 for x in range(len(allsamps))]
+
+		#Turn this dictionary into a DataFrame
+		rowdf = pd.DataFrame.from_dict(d)
+
+		#Get LME pvalue, but only if we haven't already determined that the pvalue is NA because we are missing one or more psi values
+		#Lots of warnings about convergence, etc. Suppress them.
+		if not p:
+			with warnings.catch_warnings():
+				warnings.filterwarnings('ignore')
+
+				#So apparently, some combinations of psi values will give nan p values due to a LinAlgError that arises from a singular
+				#hessian matrix during the fit of the model.  However, the equivalent code in R (nlme::lme) never gives this error, even with
+				#the same data. It's not clear from just looking at the psi values why this is.  However, I found that by varying the 
+				#start_params in the fit, this can be avoided. If this is done, the resulting p value always matches what is given in R.
+				#Further, the p value is the same regardless of the start_param.
+				#But it's not clear to me why changing the start_param matters, or what the default is here or with nlme.
+				#So let's try a few starting paramters.  Regardless, this seems to affect a small number of genes (<1%), and it is causing 
+				#false negatives because genes that should get p values (may or may not be sig) are getting NA.
+				possible_start_params = [0, 1, -1, 2, -2, -3, 3]
+				numberoftries = -1
+				for param in possible_start_params:
+					#if we already have a pvalue, don't try again
+					if p != None and not np.isnan(p):
+						break
+					#First time through, numberoftries = 0, and we are just using a placeholder startparam (0) here because we aren't even using it.
+					#Gonna use whatever the default is
+					numberoftries +=1
+					try:
+						#actual model
+						md = smf.mixedlm('value ~ cond1', data = rowdf, groups = 'samples', missing = 'drop')
+						if numberoftries == 0:
+							mdf = md.fit(reml = False) #REML needs to be false in order to use log-likelihood for pvalue calculation
+						elif numberoftries > 0:
+							mdf = md.fit(reml = False, start_params = [param])
+
+						#null model
+						nullmd = smf.mixedlm('value ~ 1', data = rowdf, groups = 'samples', missing = 'drop')
+						if numberoftries == 0:
+							nullmdf = nullmd.fit(reml = False)
+						elif numberoftries > 0:
+							nullmdf = nullmd.fit(reml = False, start_params = [param])
+
+						#Likelihood ratio
+						LR = 2 * (mdf.llf - nullmdf.llf)
+						p = chi2.sf(LR, df = 1)
+
+					#These exceptions are needed to catch cases where either all psi values are nan (valueerror) or all psi values for one condition are nan (linalgerror)
+					except (ValueError, np.linalg.LinAlgError):
+						p = np.nan
+
+		pvaluedict[index] = float('{:.2e}'.format(p))
+
+	#Correct pvalues using BH method, but only using pvalues that are not NA
+	pvalues = list(pvaluedict.values())
+	pvaluesformultitest = [pval for pval in pvalues if str(pval) != 'nan']
+	fdrs = list(multipletests(pvaluesformultitest, method = 'fdr_bh')[1])
+	fdrs = [float('{:.2e}'.format(fdr)) for fdr in fdrs]
+
+	#Now we need to incorporate the places where p = NA into the list of FDRs (also as NA)
+	fdrswithnas = []
+	fdrindex = 0
+	for pvalue in pvalues:
+		#print(pvalue)
+		if str(pvalue) != 'nan':
+			fdrswithnas.append(fdrs[fdrindex])
+			fdrindex +=1
+		elif str(pvalue) == 'nan':
+			fdrswithnas.append(np.nan)
 
 
+	fdrdict = dict(zip(list(pvaluedict.keys()), fdrswithnas))
+
+	#Turn into a df
+	pdf = pd.DataFrame.from_dict(fdrdict, orient = 'index')
+	pdf.columns = ['FDR']
+	#Add means and MADs
+	pdf = pdf.assign(condamean = condameans)
+	pdf = pdf.assign(condbmean = condbmeans)
+	pdf = pdf.assign(condamad = condamads)
+	pdf = pdf.assign(condbmad = condbmads)
+
+	#rename columns
+	pdf.columns = ['FDR', '{0}_mean'.format(conditionA), '{0}_mean'.format(conditionB), '{0}_MAD'.format(conditionA), '{0}_MAD'.format(conditionB)]
+
+	return pdf
 
 
-
-#Take 'sampconds' list of UMI count files <done>
-#collate ntcoverage dicts <done>
-#Take log2FC across condition of medians across replicates
-#Repeat with 1000 shuffles
-#Calculate empirical p value
-
+#TODO
+#add covariates to LME
 #To define "regions":
 #start at sig nt, allow max gap of <gap> nonsig nt
 
@@ -511,7 +688,8 @@ smoothntdf.to_csv(path_or_buf = 'bigdf_qn_nt_smooth.txt', sep = '\t', header = T
 
 countdf = pd.read_csv('bigdf_qn_nt_smooth.txt', sep = '\t', index_col = 'position', header = 0)
 log2fcdf = getlog2fc(countdf, sys.argv[1], sys.argv[2], sys.argv[3])
-pvaldf = getpvalues_MADstat(countdf, sys.argv[1], sys.argv[2], sys.argv[3])
+#pvaldf = getpvalues_MADstat(countdf, sys.argv[1], sys.argv[2], sys.argv[3])
+pvaldf = getpvalues_LME(countdf, sys.argv[1], sys.argv[2], sys.argv[3])
 
 log2fc_and_pvaldf = pd.merge(log2fcdf, pvaldf, left_index = True, right_index = True)
-log2fc_and_pvaldf.to_csv(path_or_buf = 'log2fc_and_pvaldf.txt', sep = '\t', header = True, index = True, index_label = 'position')
+log2fc_and_pvaldf.to_csv(path_or_buf = 'log2fc_and_pvaldf.txt', sep = '\t', header = True, index = True, index_label = 'position', float_format = '%.3g')
